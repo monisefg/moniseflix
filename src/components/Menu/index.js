9c5600076bbe8235843f9f45e12d73bca23ca10a
Monise F. Gomes <monise.fg@gmail.com>
2020-08-01 18:51:17 -0300

    Alteração da descrição do vídeo do banner

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.png";
-import "./Menu.css";
-//import ButtonLink from "../ButtonLink";
-import Button from "../Button";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+import './Menu.css';
+// import ButtonLink from "../ButtonLink";
+import Button from '../Button';
 
 function Menu() {
   return (
     <header>
       <nav className="Menu">
         <Link to="/">
-          <img className="Logo" src={Logo} alt="Moniseflix Logo"></img>
+          <img className="Logo" src={Logo} alt="Moniseflix Logo" />
         </Link>
 
         <Button as={Link} className="ButtonLink" to="/cadastro/video">
